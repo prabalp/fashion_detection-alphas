751ea7ef073f51d5f6f1f85c05004f1814cdc0f8
prabalp <prabalpanda2008@gmail.com>
2021-11-01 09:26:00 +0530

    clean

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import React, { useState,useEffect } from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Header from "./components/Header";
 import MainBoard from "./components/MainBoard";
 import unsplash from "./api/unsplash";
-import { PinSharp } from '@mui/icons-material';
+// import { PinSharp } from "@mui/icons-material";
 
 function App() {
-
-  const [pins, setNewPins] = useState([]) 
+  const [pins, setNewPins] = useState([]);
 
   const getImages = (term) => {
     return unsplash.get("https://api.unsplash.com/search/photos", {
       params: {
-        query: term
-      }
+        query: term,
+      },
     });
-  }
-
-
+  };
 
   const onSearchSubmit = (term) => {
-    console.log("on search submit", term)
+    console.log("on search submit", term);
     getImages(term).then((res) => {
       let results = res.data.results;
 
-      let newPins = [
-        ...results,
-        ...pins,
-      ]
+      let newPins = [...results, ...pins];
 
-      newPins.sort(function(a,b) {
+      newPins.sort(function (a, b) {
         return 0.5 - Math.random();
       });
 
       setNewPins(newPins);
-    })
-  }
+    });
+  };
 
   const getNewPins = () => {
     let promises = [];
     let pinData = [];
 
-    let pins = ['ocean', 'Tokyo', 'dogs', 'cats', 'Bali' ];
+    let pins = ["ocean", "Tokyo", "dogs", "cats", "Bali"];
 
     pins.forEach((pinTerm) => {
       promises.push(
@@ -51,27 +44,26 @@ function App() {
 
           pinData = pinData.concat(results);
 
-          pinData.sort(function (a,b) {
+          pinData.sort(function (a, b) {
             return 0.5 - Math.random;
           });
         })
       );
     });
-    
+
     Promise.all(promises).then(() => {
       setNewPins(pinData);
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-
-    getNewPins()
-  },  [])
+    getNewPins();
+  });
 
   return (
     <div className="app">
-        <Header onSubmit={onSearchSubmit}/>
-        <MainBoard pins={pins} />
+      <Header onSubmit={onSearchSubmit} />
+      <MainBoard pins={pins} />
     </div>
   );
 }
